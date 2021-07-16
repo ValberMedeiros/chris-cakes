e7c1b8002bc65d989ddf8a4c67e0f37eeac7b217
ValberMedeiros <dev.junior.medeiros@gmail.com>
2021-07-15 22:14:55 -0300

    Repositórios de usuarios e roles

---
+++ src/main/java/com/br/chriscakes/repositories/RoleRepository.java
@@ -0,0 +1,9 @@
+package com.br.chriscakes.repositories;
+
+import com.br.chriscakes.domain.entities.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Long> {
+}
