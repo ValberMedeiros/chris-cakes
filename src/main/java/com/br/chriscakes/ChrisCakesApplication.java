a4b41acc80aa811dcc963fe079c3d61d55cf2903
ValberMedeiros <dev.junior.medeiros@gmail.com>
2021-07-15 22:13:38 -0300

    Initial Commit!

---
+++ src/main/java/com/br/chriscakes/ChrisCakesApplication.java
@@ -0,0 +1,13 @@
+package com.br.chriscakes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChrisCakesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChrisCakesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/br/chriscakes/ChrisCakesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.chriscakes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChrisCakesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
